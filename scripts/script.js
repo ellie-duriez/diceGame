3c36d5ea169555115c6bfdec533546df1a224e73
Ellie Duriez <ellieduriez95@gmail.com>
2022-04-03 20:25:41 +0100

    javascript added for cumulative total calculation and display to user

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
 const rollPlayer1 = document.getElementById("dice1");
 const rollPlayer2 = document.getElementById("dice2");
+let currScorePlayer1 = document.getElementById("currentScore1");
+let currScorePlayer2 = document.getElementById("currentScore2");
+let totScorePlayer1 = document.getElementById("totalScore1");
+let totScorePlayer2 = document.getElementById("totalScore2");
+let trackScorePlayer1 = 0;
+let trackScorePlayer2 = 0;
 
 const randNum = () => {
 return Math.ceil(Math.random() * 6);
@@ -19,11 +25,12 @@ rollPlayer1.addEventListener("mousedown", (event) => {
 resetVisibility("1");
 // ROLL THE DICE BETWEEN 1 - 6
 let diceRoll1 = randNum();
-alert(diceRoll1);
 // CHANGE DICE VISUALS PROP TO DOT
 changeVisuals("1",diceRoll1);
 // DISPLAY CURRENT ROLL SCORE
-
+currScorePlayer1.innerText = diceRoll1;
 // DISPLAY CUMULATIVE TOTAL SCORE
-
+trackScorePlayer1 += diceRoll1;
+alert(trackScorePlayer1);
+totScorePlayer1.innerText = trackScorePlayer1;
 })
