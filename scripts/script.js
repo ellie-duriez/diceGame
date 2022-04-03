1b27d74be4aa698da6f5f8d57adfca667d099e98
Ellie Duriez <ellieduriez95@gmail.com>
2022-04-04 00:19:32 +0100

    game is now functional in a basic sense. the 1 player game is playable. scores are output to screen etc and game is reset after win/lose. but many tweaks required.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// PREDEFINING GLOBAL VARIBALES
 const rollPlayer1 = document.getElementById("dice1");
 const rollPlayer2 = document.getElementById("dice2");
 let currScorePlayer1 = document.getElementById("currentScore1");
@@ -7,6 +8,7 @@ let totScorePlayer2 = document.getElementById("totalScore2");
 let trackScorePlayer1 = 0;
 let trackScorePlayer2 = 0;
 
+// PREDEFINING FUNCTIONS
 const randNum = () => {
 return Math.ceil(Math.random() * 6);
 }
@@ -20,17 +22,41 @@ const changeVisuals = (playerNum, diceRoll) => {
     document.getElementById(`dice${playerNum}dot${diceRoll}`).style.visibility = "visible";
 }
 
+// playAgain.addEventListener("mousedown", () => {
+// currScorePlayer1.innerText = "";
+// trackScorePlayer1 = 0;
+// totScorePlayer1.innerText = "0";
+// })
+
+// DICE ROLL FOR PLAYER 1
 rollPlayer1.addEventListener("mousedown", (event) => {
-// RESET VISUALS
-resetVisibility("1");
-// ROLL THE DICE BETWEEN 1 - 6
-let diceRoll1 = randNum();
-// CHANGE DICE VISUALS PROP TO DOT
-changeVisuals("1",diceRoll1);
-// DISPLAY CURRENT ROLL SCORE
-currScorePlayer1.innerText = diceRoll1;
-// DISPLAY CUMULATIVE TOTAL SCORE
-trackScorePlayer1 += diceRoll1;
-alert(trackScorePlayer1);
-totScorePlayer1.innerText = trackScorePlayer1;
+    // RESET VISUALS
+    resetVisibility("1");
+    // ROLL THE DICE BETWEEN 1 - 6
+    let diceRoll1 = randNum();
+    // CHANGE DICE VISUALS PROP TO DOT
+    changeVisuals("1",diceRoll1);
+    // DISPLAY CURRENT ROLL SCORE
+    currScorePlayer1.append(diceRoll1," ");
+    // DISPLAY CUMULATIVE TOTAL SCORE
+    trackScorePlayer1 += diceRoll1;
+    totScorePlayer1.innerText = trackScorePlayer1;
+    // CHECK SCORE FOR WIN/LOSE
+    if (trackScorePlayer1 >= 30) {
+        // document.getElementById("playAgain").style.display = "show";
+        alert("You win!");
+        resetVisibility("1")
+        currScorePlayer1.innerText = "";
+        trackScorePlayer1 = 0;
+        totScorePlayer1.innerText = "0";
+    }
+
+    if (diceRoll1 == 1) {
+        // document.getElementById("playAgain").style.display = "show";
+        alert("You rolled a 1! Too bad, game over.");
+        resetVisibility("1")
+        currScorePlayer1.innerText = "";
+        trackScorePlayer1 = 0;
+        totScorePlayer1.innerText = "0";
+    }
 })
