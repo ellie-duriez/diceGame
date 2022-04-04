d60eb61e3674e938ca940d962248347516deb64b
Ellie Duriez <ellieduriez95@gmail.com>
2022-04-04 23:47:07 +0100

    added a 'play again' button

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,8 @@ let totScorePlayer1 = document.getElementById("totalScore1");
 let totScorePlayer2 = document.getElementById("totalScore2");
 let trackScorePlayer1 = 0;
 let trackScorePlayer2 = 0;
+let diceRoll1;
+let playAgain = document.getElementById("playAgain");
 
 // PREDEFINING FUNCTIONS
 const randNum = () => {
@@ -23,42 +25,53 @@ const changeVisuals = (playerNum, diceRoll) => {
     document.getElementById(`dice${playerNum}dot${diceRoll}`).style.visibility = "visible";
 }
 
-// playAgain.addEventListener("mousedown", () => {
-// currScorePlayer1.innerText = "";
-// trackScorePlayer1 = 0;
-// totScorePlayer1.innerText = "0";
-// })
+
 
 // DICE ROLL FOR PLAYER 1
-rollPlayer1.addEventListener("mousedown", (event) => {
-    // RESET VISUALS
-    resetVisibility("1");
-    // ROLL THE DICE BETWEEN 1 - 6
-    let diceRoll1 = randNum();
-    // CHANGE DICE VISUALS PROP TO DOT
-    changeVisuals("1",diceRoll1);
-    // DISPLAY CURRENT ROLL SCORE
-    currScorePlayer1.append(diceRoll1," ");
-    // DISPLAY CUMULATIVE TOTAL SCORE
-    trackScorePlayer1 += diceRoll1;
-    totScorePlayer1.innerText = trackScorePlayer1;
+diceRollFunc1 = (func1) => {
+    rollPlayer1.addEventListener("mousedown", (event) => {
+        // RESET VISUALS
+        resetVisibility("1");
+        // ROLL THE DICE BETWEEN 1 - 6
+        diceRoll1 = randNum();
+        // CHANGE DICE VISUALS PROP TO DOT
+        changeVisuals("1",diceRoll1);
+        // DISPLAY CURRENT ROLL SCORE
+        currScorePlayer1.append(diceRoll1," ");
+        // DISPLAY CUMULATIVE TOTAL SCORE
+        trackScorePlayer1 += diceRoll1;
+        totScorePlayer1.innerText = trackScorePlayer1;
+        func1()
+    })
+}
+
     // CHECK SCORE FOR WIN/LOSE
-    if (trackScorePlayer1 >= 30) {
-        // document.getElementById("playAgain").style.display = "show";
-        alert("You win!");
-        resetVisibility("1")
-        currScorePlayer1.innerText = "";
-        trackScorePlayer1 = 0;
-        totScorePlayer1.innerText = "0";
+    checkScore1 = () => {
+        if (trackScorePlayer1 >= 30) {
+            // document.getElementById("playAgain").style.display = "show";
+            alert("You win!");
+            playAgain.style.visibility = "visible";
+            playAgain.addEventListener("mousedown", () => {
+                resetVisibility("1")
+                currScorePlayer1.innerText = "";
+                trackScorePlayer1 = 0;
+                totScorePlayer1.innerText = "0";
+                })
+        }
+
+        else if (diceRoll1 == 1) {
+            // document.getElementById("playAgain").style.display = "show";
+            alert("You rolled a 1! Too bad, game over.");
+            playAgain.style.visibility = "visible";
+            playAgain.addEventListener("mousedown", () => {
+                resetVisibility("1")
+                currScorePlayer1.innerText = "";
+                trackScorePlayer1 = 0;
+                totScorePlayer1.innerText = "0";
+                })
+        }
     }
 
-    if (diceRoll1 == 1) {
-        // document.getElementById("playAgain").style.display = "show";
-        alert("You rolled a 1! Too bad, game over.");
-        resetVisibility("1")
-        currScorePlayer1.innerText = "";
-        trackScorePlayer1 = 0;
-        totScorePlayer1.innerText = "0";
-    }
-})
+//FUNCTION TO KICKSTART THE GAME
+diceRollFunc1(checkScore1);
 
